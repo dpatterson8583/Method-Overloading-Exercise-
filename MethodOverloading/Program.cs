226a57a4e04cc1689f43a91c451b7e77516d6398
dpatterson8583 <dpatterson8583@gmail.com>
2023-04-19 18:44:29 -0500

    Initial commit for method overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer1 = Add(2, 3);
+            Console.WriteLine(answer1);
+
+            var answer2 = Add(232445m, .284m);
+            Console.WriteLine(answer2);
+
+            var answer3 = Add(10, 100, true);
+            Console.WriteLine(answer3);
         }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal  x, decimal y) 
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool isTrue)
+        {
+            if (isTrue) 
+            { 
+                if(x+y==1)
+                {
+                    return "1 dollar ";
+                }
+                else
+                {
+                    return Convert.ToString(x + y) + " dollars";
+
+                }
+
+            }
+            else 
+            {
+                return Convert.ToString(x + y);
+            }
+        }
+
     }
 }
